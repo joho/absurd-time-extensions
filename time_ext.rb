9bf5db1900b873312f3bd276a5c43852930a57ad
John Barton (joho) <jrbarton@gmail.com>
2008-07-01 11:35:16 +1000

    Putting in a few classics, like beer oclock and that shitty swatch internet time thing

---
+++ time_ext.rb
@@ -0,0 +1,23 @@
+class Time
+  # stupid swatch and their dotcom boom digital wristwatches
+  def to_beats
+    seconds, minutes, hours, *everything_else = getutc.to_a
+    (((hours + 1) * 3600 + (minutes * 60) + seconds) / 86.4).to_i
+  end
+  
+  def is_beer_oclock?
+    true
+  end
+  
+  # handling the vanilla ice/mc hammer event
+  attr_accessor :white_rapper
+  def stop!
+    if white_rapper
+      "collaborate and listen"
+    else
+      "Hammertime!"
+    end
+  end
+end
+
+puts Time.now.to_beats
